cb2fc397c14632c0f13a1ab3a75b41d2fe5af9a9
fhj96 <1483043597@qq.com>
2022-07-05 17:48:07 +0800

    登录页面

--- src/main.js
+++ src/main.js
@@ -3,7 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "@/styles/index.less";
+import TouTiaoIcon from "@/components/TouTiaoIcon";
+import vant from "vant";
+import "vant/lib/index.less";
+Vue.use(vant);
+import "amfe-flexible";
+// 测试接口
+import request from "@/utils/request";
+request.get("/v1_0/announcements").then((res) => {
+  console.log(res);
+});
 
+Vue.component("TouTiaoIcon", TouTiaoIcon);
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/Login/index.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar title="登录" />
+    <van-form @submit="onSubmit" ref="form">
+      <van-field
+        v-model="user.mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="rules.mobile"
+        required
+        type="number"
+        maxlength="11"
+      >
+        <template #left-icon>
+          <TouTiaoIcon icon="shouji"></TouTiaoIcon>
+        </template>
+      </van-field>
+      <van-field
+        v-model="user.code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="rules.code"
+        required
+        type="number"
+        maxlength="6"
+      >
+        <template #left-icon>
+          <TouTiaoIcon icon="yanzhengma"></TouTiaoIcon>
+        </template>
+
+        <template #button>
+          <van-count-down
+            @finish="isShowCountDown = false"
+            :time="5000"
+            format="ss s"
+            v-if="isShowCountDown"
+          />
+          <van-button
+            v-else
+            @click="sendSmsCode"
+            class="send-sms-btn"
+            size="small"
+            native-type="button"
+            :loading="isdisabled"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+import { login, getSmsCode } from "@/api/user";
+import { Toast } from "vant";
+export default {
+  name: "LoginPage",
+  components: {},
+  props: {},
+  data() {
+    return {
+      isdisabled: false,
+      isShowCountDown: false,
+      user: {
+        mobile: "13911111112",
+        code: "246810",
+      },
+      rules: {
+        mobile: [
+          {
+            required: true,
+            message: "手机号不能为空",
+          },
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: "手机号格式不对",
+          },
+        ],
+        code: [
+          {
+            required: true,
+            message: "验证码不能为空",
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: "验证码只能为6位",
+          },
+        ],
+      },
+    };
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    async onSubmit() {
+      try {
+        const res = await login(this.user);
+        console.log(res);
+        Toast.success("登录成功");
+      } catch (e) {
+        console.log(e);
+        Toast.fail(e?.response?.data?.message || "服务器端错误");
+      }
+    },
+    async sendSmsCode() {
+      try {
+        await this.$refs.form.validate("mobile");
+      } catch (e) {
+        console.log(e);
+        return;
+      }
+      try {
+        this.isdisabled = true;
+        await getSmsCode(this.user.mobile);
+        this.isShowCountDown = true;
+        Toast.success("发送验证码成功！");
+      } catch (e) {
+        console.log(e);
+        Toast.fail(e?.response?.data?.message || "报错了");
+        this.isShowCountDown = false;
+      } finally {
+        this.isdisabled = false;
+      }
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.login-container {
+  .toutiao {
+    font-size: 37px;
+  }
+}
+.send-sms-btn {
+  width: 165px;
+  height: 46px;
+  line-height: 46px;
+  background-color: #ededed;
+  font-size: 22px;
+  color: #666;
+  border: 1px solid #07c160;
+}
+</style>
